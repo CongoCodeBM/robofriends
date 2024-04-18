ff071ea0785bec0840df13f14a0f68a04a0bb9bd
CongoCodeBM <bnmunganga.bnm@gmail.com>
2024-04-18 13:43:53 -0700

    Updates

---
+++ precache-manifest.7e880c07568280d772c7f3fff76c39bb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5579ee0a4a1dc2969dfccbfad62bb9d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6daf001339a35142e01",
+    "url": "/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "84694fc23a5221cab733",
+    "url": "/static/css/main.6b5b021a.chunk.css"
+  },
+  {
+    "revision": "e6daf001339a35142e01",
+    "url": "/static/js/2.36f9ee18.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.36f9ee18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ecc8d4759925795b22",
+    "url": "/static/js/3.7f0ccf67.chunk.js"
+  },
+  {
+    "revision": "84694fc23a5221cab733",
+    "url": "/static/js/main.39186151.chunk.js"
+  },
+  {
+    "revision": "c78e916aeaa97c01b4a2",
+    "url": "/static/js/runtime-main.e3844591.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
